e3e448f7a85cfef3bd7bc65b1ef0e12ab64ca683
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-08-11 19:56:40 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """print all names defined by hidden_4 module."""
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-    if name[:2] != "__":
-        print(name)
+    n = dir(hidden_4)
+    for n1 in n:
+    if n1[:2] != "__":
+        print(n1)
