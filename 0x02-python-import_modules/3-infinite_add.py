97371b300e5d036d21891f5612391284944907bb
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-08-11 19:13:01 +0000

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Prints the sum of arguments"""
+    import sys
+
+    add = 0
+    for x in range(len(sys.argv) - 1)
+        add += int(sys.argv[x + 1])
+    print("{}".format(add))
