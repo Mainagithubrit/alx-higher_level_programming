8f19033a31e2af1ab44a45126ab4764eec612471
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-08-28 17:38:28 +0000

    Task: 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    rtn = 0
+    for y in range(x):
+        try:
+            print("{}".format(my_list[y]), end="")
+            rtn += 1
+        except IndexError:
+            break
+    print("")
+    return (rtn)
