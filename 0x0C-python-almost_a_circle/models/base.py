585f850a081d3d40d939c8d9c4e25737b2fb0778
Mainagithubrit <njorogefrancismaina@gmail.comgit config --global user.email njorogefrancismaina@gmail.com>
2023-10-16 21:38:24 +0000

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,14 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (string): a string representing a list of dictionaries
+        """
+        if json_string is None or json_string == "[]"
+        return []
+        return json.loads(json_string)
