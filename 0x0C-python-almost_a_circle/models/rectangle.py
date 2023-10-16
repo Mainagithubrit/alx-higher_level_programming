2d9950e765ebbc83d195b890ba6a42050c16fe27
Mainagithubrit <njorogefrancismaina@gmail.comgit config --global user.email njorogefrancismaina@gmail.com>
2023-10-16 20:15:01 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -146,3 +146,13 @@ class Rectangle(Base):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a rectangle"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height" self.height,
+            "x": self.x,
+            "y": self.y
+        }
