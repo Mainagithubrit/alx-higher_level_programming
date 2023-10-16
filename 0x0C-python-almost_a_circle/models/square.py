0d3ce58644d784073a2a208265ea8be4e0c6417a
Mainagithubrit <njorogefrancismaina@gmail.comgit config --global user.email njorogefrancismaina@gmail.com>
2023-10-16 21:14:11 +0000

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -72,3 +72,12 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Returns the representation of a square in dictionary form"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
