0fbfe01733d5cfd9aefa4df0dfd1a7c0c987a266
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-01-20 00:10:40 +0300

    16-shows_by_genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
-LEFT tv_genres ON tv_show_genres.genre_id=tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
 ORDER BY title, tv_genres.name;
