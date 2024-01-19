5f037f796a608cfbc781629a674d6cdd07a07721
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-01-19 22:44:05 +0300

    9-cities_of_california_subquery

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, stats.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 join states ON cities.state_id=states.id
 ORDER BY cities.id;
