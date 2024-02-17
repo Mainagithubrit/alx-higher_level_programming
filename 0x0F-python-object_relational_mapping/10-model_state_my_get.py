12c7b0d00e998ac58d536cf521eabd9f1a05a821
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-17 23:34:36 +0300

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 
 """a script that prints the State object with the name
 passed as argument from the database hbtn_0e_6_usa"""
@@ -12,11 +12,11 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_engine(engine)
+  Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).filter(State.name == (sys.argv[4],))
     try:
         print(instance[0].id)
     except IndexError:
-        print('Not found')
+        print("Not found")  
