896c48a6adb296e971f4fcc0446da32c7d1881ab
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-04-03 23:30:54 +0300

    Updated

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,4 @@
 // a JavaScript script that updates the text
 // color of the <header> element to red (#FF0000)
 
-document.querySelector('head').style.color = '#FF0000';
-
+document.querySelector('HEADER').style.color = '#FF0000';
